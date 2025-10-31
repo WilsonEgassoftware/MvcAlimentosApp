6297ba3f0e6b28542d1d5cb94d55cb186ca307da
WilsonEgassoftware <146997073+WilsonEgassoftware@users.noreply.github.com>
2025-10-31 16:33:57 -0500

    Add files via upload

---
+++ Views/Alimentos/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MvAlimentosApp.VIsta.Alimentos
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
