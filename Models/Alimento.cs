63d25a68803395fdc3ab7c2322289676a1792410
WilsonEgassoftware <146997073+WilsonEgassoftware@users.noreply.github.com>
2025-10-31 16:36:46 -0500

    Add files via upload

---
+++ Models/Alimento.cs
@@ -0,0 +1,10 @@
+﻿namespace MvcAlimentosApp.Models
+{
+    public class Alimento
+    {
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public string? Categoria { get; set; }
+        public string? Empresa { get; set; }
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,11 @@
+﻿namespace MvcAlimentosApp.Models
+{
+    public class Usuario
+    {
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+
+        public string? Rol { get; set; }
+
+    }
+}
