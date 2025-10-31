f3fc9436bd2f93520f7f989b9138aea2fde8642a
WilsonEgassoftware <146997073+WilsonEgassoftware@users.noreply.github.com>
2025-10-31 16:37:03 -0500

    Add files via upload

---
+++ Express/express.jsx
@@ -0,0 +1,14 @@
+﻿import React from "react";
+import ReactDOM from "react-dom/client";
+
+function ExpressApp() {
+    return (
+        <div style={{ textAlign: "center", backgroundColor: "#e0f7fa", padding: "30px" }}>
+            <h2>Panel de Alimentos con React + Express.jsx 🚀</h2>
+            <p>Esta vista está renderizada dentro de ASP.NET MVC.</p>
+        </div>
+    );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("react-root"));
+root.render(<ExpressApp />);
