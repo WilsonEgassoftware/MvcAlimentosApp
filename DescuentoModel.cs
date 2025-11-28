6694c16f5c9995df67d1f73ac26386c069684ef1
WilsonEgassoftware <146997073+WilsonEgassoftware@users.noreply.github.com>
2025-11-27 21:40:44 -0500

    Add files via upload

---
+++ DescuentoModel.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MvAlimentosApp.Pages.Shared
+{
+    public class DescuentoModel : PageModel
+    {
+        public void OnGet()
+        {
+            // No se requiere lógica adicional para la vista actual
+        }
+    }
+}
