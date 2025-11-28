b86e8a432444dfd18284d2404399539c3164f49f
WilsonEgassoftware <146997073+WilsonEgassoftware@users.noreply.github.com>
2025-11-27 21:40:11 -0500

    Add files via upload

--- Descuento.cs
+++ Descuento.cs
@@ -2,8 +2,11 @@
 {
     public class Descuento
     {
-        public string? NombreAlimento { get; set; }
-        public string ?Empresa { get; set; }
-        public int? Porcentaje { get; set; }
+        public string NombreAlimento { get; set; }
+        public string Empresa { get; set; }
+        public int Porcentaje { get; set; }
+        
+        public string? Descuento { get; set; }
     }
 }
+
